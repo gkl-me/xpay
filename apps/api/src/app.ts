106343e3b25993fedbe82b562fd9e3240255c20e
Gokul <gklgrme@gmail.com>
2026-02-13 21:59:21 +0530

    chore:backend init

---
+++ apps/api/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { prisma } from "@repo/db";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    console.log("Received request on /");
+    try {
+        const users = await prisma.user.create({
+            data: {
+                email: `test-${Date.now()}@gmail.com`,
+            }
+        });
+        console.log("User created:", users);
+        res.send(users);
+    } catch (error: any) {
+        console.error("Error creating user:", error);
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Server started on port 3001");
+});
